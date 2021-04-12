a627f92a37faf39602086528d1ccb1ba25902d27
RoySKwon <roykwonfirmen@gmail.com>
2021-04-12 14:18:11 +0200

    created base code

---
+++ fn.test.js
@@ -0,0 +1,17 @@
+const fn = require("./fn");
+
+test("1 is 1", () => {
+  expect(1).toBe(1);
+});
+
+test("2 plus 3 is 5", () => {
+  expect(fn.add(2, 3)).toBe(5);
+});
+
+test("4 plus 5 ist 10", () => {
+  expect(fn.add(4, 5)).toBe(10);
+});
+
+test("5 plut 6 is 10", () => {
+  expect(fn.add(5, 6)).toBe(10);
+});
